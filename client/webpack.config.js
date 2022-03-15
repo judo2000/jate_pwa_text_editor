55bd8919de4b2bce8264d7518d7c120a11be9746
Stephen Moore <judo2000@comcast.net>
2022-03-15 16:53:56 -0600

    Add id to webpack manifext plugin so install pwa works

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -30,6 +30,7 @@ module.exports = () => {
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "/",
+        id: 1,
         publicPath: "/",
         icons: [
           {
